a387e9ece2f01546c13e454769ae91b63f6a70ff
Amit K. Yadav <107500406+amit8585@users.noreply.github.com>
2022-07-27 23:05:49 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+  char op;
+  double first, second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter first operands: ");
+  scanf("%lf", &first);
+  printf("Enter second operands: ");
+  scanf("%lf", &second);
+
+  switch (op) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      break;
+
+
