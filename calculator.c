5c6aa8afb69f145ca4ddc2121015690a18a4e936
Amit K. Yadav <107500406+amit8585@users.noreply.github.com>
2022-07-27 23:12:11 +0530

    Adding third operator

--- calculator.c
+++ calculator.c
@@ -18,6 +18,9 @@ int main() {
     case '-':
       printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
       break;
-   
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      break;
+  
 
 
