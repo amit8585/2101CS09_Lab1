24a543a4f1cdc213d6ccd59314c0e7b4d157f103
Amit K. Yadav <107500406+amit8585@users.noreply.github.com>
2022-07-27 23:27:30 +0530

    Adding Bubbling Sort

--- sorting.c
+++ sorting.c
@@ -107,3 +107,22 @@ void mergeSort(int arr[], int l, int r)
         merge(arr, l, m, r);
     }
 }
+
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+}
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+  
+        // Last i elements are already 
+        // in place
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+}
+  
