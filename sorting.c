b72d18142d680c51451bf60e04a0d880c146ce90
Amit K. Yadav <107500406+amit8585@users.noreply.github.com>
2022-07-27 23:30:18 +0530

    Final Sorting

--- sorting.c
+++ sorting.c
@@ -158,4 +158,46 @@ void insert(int a[], int n)
         a[j+1] = temp;    
     }  
 }  
+
+void insert(int a[], int n)
+{  
+    int i, j, temp;  
+    for (i = 1; i < n; i++) {  
+        temp = a[i];  
+        j = i - 1;  
+  
+        while(j>=0 && temp <= a[j]) 
+        {    
+            a[j+1] = a[j];     
+            j = j-1;    
+        }    
+        a[j+1] = temp;    
+    }  
+}  
+  
+void printArr(int a[], int n) 
+{  
+    int i;  
+    for (i = 0; i < n; i++)  
+        printf("%d ", a[i]);  
+}  
+  
+int main()  
+{  
+    int a[] = { 12, 31, 25, 8, 32, 17 };  
+    int n = sizeof(a) / sizeof(a[0]);  
+    printf("Before sorting array elements are - \n");  
+    printArr(a, n);  
+    //insert(a, n);  
+  //selectionSort(a, n);
+   // bubbleSort(arr, N);
+    //    mergeSort(a, 0, n - 1); 
+    quickSort(a, 0, n - 1);
+    printf("\nAfter sorting array elements are - \n");    
+    printArr(a, n);  
+  
+    return 0;  
+}
+
+
   
