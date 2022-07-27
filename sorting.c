54a0ed4f0fc551cbfa68bd3b0673bfa468eacab1
Amit K. Yadav <107500406+amit8585@users.noreply.github.com>
2022-07-27 23:28:18 +0530

    Adding selection sort

--- sorting.c
+++ sorting.c
@@ -126,3 +126,20 @@ void bubbleSort(int arr[], int n)
                 swap(arr[j], arr[j + 1]);
 }
   
+void selectionSort(int arr[], int n) 
+{ 
+    int i, j, min_idx; 
+  
+    // One by one move boundary of 
+    // unsorted subarray 
+    for (i = 0; i < n-1; i++) 
+    { 
+       
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+        if (arr[j] < arr[min_idx]) 
+            min_idx = j;
+        swap(&arr[min_idx], &arr[i]); 
+    } 
+} 
+  
