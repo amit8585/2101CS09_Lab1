1c08e73970e6ec230cd3798172d931719d340de7
Amit K. Yadav <107500406+amit8585@users.noreply.github.com>
2022-07-27 23:29:22 +0530

    Adding Insertion sort

--- sorting.c
+++ sorting.c
@@ -142,4 +142,20 @@ void selectionSort(int arr[], int n)
         swap(&arr[min_idx], &arr[i]); 
     } 
 } 
+
+void insert(int a[], int n)
+{  
+    int i, j, temp;  
+    for (i = 1; i < n; i++) {  
+        temp = a[i];  
+        j = i - 1;  
+  
+        while(j>=0 && temp <= a[j]) 
+        {    
+            a[j+1] = a[j];     
+            j = j-1;    
+        }    
+        a[j+1] = temp;    
+    }  
+}  
   
